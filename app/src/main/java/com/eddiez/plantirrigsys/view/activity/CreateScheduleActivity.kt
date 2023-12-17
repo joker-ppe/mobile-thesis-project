66b7347a115af213a1b1367d7853714155e0346b
Joker pp_e <voix.momotalk@gmail.com>
2023-12-18 00:00:56 +0700

    Updated: Create Schedule Activity

--- app/src/main/java/com/eddiez/plantirrigsys/view/activity/CreateScheduleActivity.kt
+++ app/src/main/java/com/eddiez/plantirrigsys/view/activity/CreateScheduleActivity.kt
@@ -2,6 +2,7 @@ package com.eddiez.plantirrigsys.view.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import com.eddiez.plantirrigsys.base.BaseActivity
@@ -9,34 +10,54 @@ import com.eddiez.plantirrigsys.databinding.ActivityCreateScheduleBinding
 
 class CreateScheduleActivity : BaseActivity() {
     private lateinit var binding: ActivityCreateScheduleBinding
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        // Callback is invoked after the user selects a media item or closes the
-        // photo picker.
-        if (uri != null) {
-            Log.d("PhotoPicker", "Selected URI: $uri")
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: $uri")
 
-            binding.imgData.setImageURI(uri)
-        } else {
-            Log.d("PhotoPicker", "No media selected")
+                binding.imgData.setImageURI(uri)
+            } else {
+                Log.d("PhotoPicker", "No media selected")
+            }
         }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateScheduleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        innitView()
+    }
+
+    private fun innitView() {
         binding.imgData.setOnClickListener {
             // Registers a photo picker activity launcher in single-select mode.
             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
 
-//            val mimeType = "image/gif"
-//            pickMedia.launch(
-//                PickVisualMediaRequest(
-//                    ActivityResultContracts.PickVisualMedia.SingleMimeType(
-//                        mimeType
-//                    )
-//                )
-//            )
+        binding.ftfTemperatureThreshold.visibility = View.GONE
+        binding.ftfMoistureThreshold.visibility = View.GONE
+
+        binding.cbTemperatureThreshold.setOnCheckedChangeListener { _, isChecked ->
+            run {
+                if (isChecked) {
+                    binding.ftfTemperatureThreshold.visibility = View.VISIBLE
+                } else {
+                    binding.ftfTemperatureThreshold.visibility = View.GONE
+                }
+            }
+        }
+
+        binding.cbMoistureThreshold.setOnCheckedChangeListener { _, isChecked ->
+            run {
+                if (isChecked) {
+                    binding.ftfMoistureThreshold.visibility = View.VISIBLE
+                } else {
+                    binding.ftfMoistureThreshold.visibility = View.GONE
+                }
+            }
         }
     }
 }
